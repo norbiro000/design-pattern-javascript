c59980e60cb0d13dac65e9b552fd8dfe50c98975
norbiro000 <s5530213065@phuket.psu.ac.th>
2019-06-30 14:10:45 +0700

    [Added] Flyweight pattern

---
+++ patterns/FlyweightPattern/index.flyweight.js
@@ -0,0 +1,87 @@
+const Todo = function (data) {
+  this.flyweight = TodoFlyweightFactory.get(data.priority, data.section, data.project, data.assignTo, data.complete)
+  this.name = data.name
+  // this.priority = data.priority
+  // this.section = data.section
+  // this.project = data.project
+  // this.assignTo = data.assignTo
+  // this.complete = data.complete
+}
+
+const TodoFlyweight = function (priority, section, project, assignTo, complete) {
+  this.priority = priority
+  this.section = section
+  this.project = project
+  this.assignTo = assignTo
+  this.complete = complete
+}
+
+const TodoFlyweightFactory = function (priority, section, project, assignTo, complete){
+  let todos = {}
+  let count = 0
+  function get (priority, section, project, assignTo, complete) {
+    if(!todos[priority + section + project + assignTo + complete]) {
+      todos[priority + section + project + assignTo + complete] = new TodoFlyweight(priority, section, project, assignTo, complete)
+      count++
+    }
+
+    return todos[priority + section + project + assignTo + complete]
+  }
+
+  function getTodoCount () {
+    return count
+  }
+
+  return {
+    get: get,
+    getTodoCount: getTodoCount
+  }
+}()
+
+function TodoCollection () {
+  let todos = {}
+  let count = 0
+
+  let add = function (todo) {
+    todos[todo.name] = new Todo(todo)
+    count += 1
+  }
+  let get = function (name) {
+    return todo[name]
+  }
+  let getCount = function () {
+    return count
+  }
+  return {
+    add: add,
+    get: get,
+    getCount: getCount
+  }
+}
+
+let todos = new TodoCollection()
+
+let priorities = ['Low', 'Medium', 'High', 'Urgent']
+let sections = ['Todo', 'Doing', 'Done']
+let projects = ['None', 'Frontend', 'Backend', 'Mobile App']
+let members = ['None', 'Jakkapan', 'Tissanu']
+let completions = ['True', 'False']
+
+var initialMemory = process.memoryUsage().heapUsed
+for(let i = 0; i < 1000000; i++ ) {
+  todos.add({
+    name: 'Todo Name' + i,
+    priority: priorities[Math.floor( Math.random() * priorities.length )],
+    section: sections[Math.floor( Math.random() * sections.length )],
+    project: projects[Math.floor( Math.random() * projects.length )],
+    assignTo: members[Math.floor( Math.random() * members.length )],
+    complete: completions[Math.floor( Math.random() * completions.length )]
+  })
+}
+
+var afterMemory = process.memoryUsage().heapUsed
+
+console.log('Used memory: ' + (afterMemory - initialMemory) / 1000000 + ' MB.')
+console.log('Todo Count: ' + todos.getCount())
+
+console.log('Todo Flyweight Count: ' + TodoFlyweightFactory.getTodoCount())
---
+++ patterns/FlyweightPattern/index.js
@@ -0,0 +1,54 @@
+const Todo = function (data) {
+  this.name = data.name
+  this.priority = data.priority
+  this.section = data.section
+  this.project = data.project
+  this.assignTo = data.assignTo
+  this.complete = data.complete
+}
+
+function TodoCollection () {
+  let todos = {}
+  let count = 0
+
+  let add = function (todo) {
+    todos[todo.name] = new Todo(todo)
+    count += 1
+  }
+  let get = function (name) {
+    return todo[name]
+  }
+  let getCount = function () {
+    return count
+  }
+  return {
+    add: add,
+    get: get,
+    getCount: getCount
+  }
+}
+
+let todos = new TodoCollection()
+
+let priorities = ['Low', 'Medium', 'High', 'Urgent']
+let sections = ['Todo', 'Doing', 'Done']
+let projects = ['None', 'Frontend', 'Backend', 'Mobile App']
+let members = ['None', 'Jakkapan', 'Tissanu']
+let completions = ['True', 'False']
+
+var initialMemory = process.memoryUsage().heapUsed
+for(let i = 0; i < 1000000; i++ ) {
+  todos.add({
+    name: 'Todo Name' + i,
+    priority: priorities[Math.floor( Math.random() * priorities.length )],
+    section: sections[Math.floor( Math.random() * sections.length )],
+    project: projects[Math.floor( Math.random() * projects.length )],
+    assignTo: members[Math.floor( Math.random() * members.length )],
+    complete: completions[Math.floor( Math.random() * completions.length )]
+  })
+}
+
+var afterMemory = process.memoryUsage().heapUsed
+
+console.log('Used memory: ' + (afterMemory - initialMemory) / 1000000 + ' MB.')
+console.log('Todo Count: ' + todos.getCount())
